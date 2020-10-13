022103890c6dac879fde6fe334b035fcbb919f96
repl.it user <replituser@example.com>
2020-10-13 10:55:35 +0000

    added additional code to multiline_message.py

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,3 +1,9 @@
 # Display message to the standard output
 print("System Failure Imminent!")
-print("System Reboot Initiated. . .")
+print("System Reboot Initiated. . .\n\n")
+
+print(". . .Rebooting Sensory System.")
+print(". . .Rebooting Output Motors.")
+print(". . .Rebooting Hover Engine.\n\n")
+
+print("System Online. Hello, User :).")
