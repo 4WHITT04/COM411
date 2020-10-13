e43c24b9a64c5877acc8c7bf7bac7769c87a924c
repl.it user <replituser@example.com>
2020-10-13 13:16:01 +0000

    added code to display ascii art

---
+++ basics/output/ascii_art.py
@@ -0,0 +1,31 @@
+# Display a Box.
+print("##########")
+print("#\t\t #")
+print("#\t\t #")
+print("##########\n\n\n")
+
+#display an Ascii image of Beep.
+print("\t\t#########")
+print("\t###           ###")
+print("#\t ---- \t\t  ----  #")
+print("#\t|    | \t\t |    | #")
+print("#\t ---- \t\t  ----  #")
+
+print("\t###           ###")
+print("\t\t#########")
+  
+print("\t\t   ###")
+print("\t\t###   ###")
+print("\t###   \t\t  ###")
+print("\t#     \t\t    #")
+print("\t#     \t\t    #")
+print("\t#     \t\t    #")
+print("\t#     \t\t    #")
+print("\t###   \t\t  ###")
+print("\t\t###   ###")
+print("\t\t   ###")
+
+
+
+
+
