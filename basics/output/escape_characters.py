b513076ed259877c892e8dbe42f2ce74e3f99efb
repl.it user <replituser@example.com>
2020-10-13 11:28:00 +0000

    added code to show a working example of escape characters.

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -3,4 +3,6 @@ print("\n Displays a new line.")
 print("\t Displays a tab space.")
 print("\\ Displays a back slash.")
 print("\" Displays a double quote.")
-print("\' Displays a single quote.")
+print("\' Displays a single quote.\n\n")
+
+print("\"Hello, I am Beep!\"")
