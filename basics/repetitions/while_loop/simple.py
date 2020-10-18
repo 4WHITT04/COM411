d9c244b34fe4c4afe718baec0412df082f144067
repl.it user <replituser@example.com>
2020-10-18 00:32:34 +0000

    Added code for while loops and while loops with count

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,17 @@
+#user input
+print("i've found a robot who's trapped under some cables! How many should i move to get them free?")
+
+cables = int(input())
+#iterating variable set to 0.
+cablegone = 0
+print()
+
+#while loop
+while (cablegone < cables):
+
+  print("Removing cables!")
+  cablegone = cablegone + 1
+
+#indication of condition fulfillment.
+print()
+print("There sure are a lot of cables here!")
