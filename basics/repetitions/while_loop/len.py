84af562892069a490e5c10ba2406855ecead222c
repl.it user <replituser@example.com>
2020-11-02 00:43:11 +0000

    added code to show len function and while loop calculations

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,13 @@
+#users will provide a word.abs
+print("Hello user, please enter a word.")
+word = input()
+
+wordcount = 0
+
+#while loop featuring len function. 
+while (wordcount < len(word)):
+
+  wordcount += 1
+
+  print("Bop.", end="")
+
---
+++ basics/repetitions/while_loop/sum_100.py
@@ -0,0 +1,19 @@
+#initial commment
+print("Now calculating the sum of numers 1 to 100")
+
+#control variable
+startingno = 1
+
+#variable for calculated sum
+sumno = 0
+print()
+
+#while loop for calculation.
+while(startingno <= 100):
+  
+  sumno = sumno + startingno
+  
+  startingno = startingno + 1 
+
+
+print("Calculations complete! The total is:", sumno, "Thats a huge number!")
